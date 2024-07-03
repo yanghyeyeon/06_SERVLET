2d16c707353e03b19006c24f4f1447f0dd51314e
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 13:14:19 +0900

    filter uses passwordencryptfilter

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/PasswordEncryptFilter.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/PasswordEncryptFilter.java
@@ -1,2 +1,37 @@
-package com.ohgiraffers.section02.uses;public class PasswordEncryptFilter {
+package com.ohgiraffers.section02.uses;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter("/member/*")
+public class PasswordEncryptFilter implements Filter {
+
+    public PasswordEncryptFilter() {
+        System.out.println("PasswordEncryptFilter 생성");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        System.out.println("암호화 필터 작동");
+        HttpServletRequest hrequest = (HttpServletRequest) servletRequest;
+
+        // requestWrapper
+        RequestWrapper wrapper = new RequestWrapper(hrequest);
+
+        filterChain.doFilter(wrapper,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
