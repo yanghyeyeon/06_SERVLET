e8c564b6ffe50404a8f048c2ca3a07a504c5a93f
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 13:14:41 +0900

    filter uses registmemberservlet

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
@@ -1,18 +1,22 @@
 package com.ohgiraffers.section02.uses;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
+
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/member/regist")
 public class RegistMemberServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+
+        System.out.println("userId = " + userId);
+        System.out.println("password = " + password);
+        System.out.println("name = " + name);
     }
 }
