bdcb497fa485433cde1a4bd1990e3b2754365ce1
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 11:52:57 +0900

    session sessionhandlerservlet

--- chap10-session-lecture-source/src/main/java/com/ohgiraffers/section01/session/SessionHandlerServlet.java
+++ chap10-session-lecture-source/src/main/java/com/ohgiraffers/section01/session/SessionHandlerServlet.java
@@ -6,15 +6,35 @@ import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
 
-@WebServlet("/")
+@WebServlet("/session")
 public class SessionHandlerServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        System.out.println("(session) firstName : " + firstName);
+        System.out.println("(session) lastName : " + lastName);
+
+//        response.sendRedirect("redirect");
+
+        // 세션 만들기
+        HttpSession session = request.getSession();
+
+        // HttpSession의 기본 유지 시간은 30분으로 지정되어있다.
+
+        // session 유지시간 설정
+        session.setMaxInactiveInterval(60*10); // 10분
+        System.out.println(" 변경후 session 유지시간 : " + session.getMaxInactiveInterval());
+
+        System.out.println("session id : " + session.getId());
+
+        session.setAttribute("firstName",firstName);
+        session.setAttribute("lastName",lastName);
+
+        response.sendRedirect("redirect");
+
 
     }
 }
