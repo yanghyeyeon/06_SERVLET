dee9f207f0c57c15aa6ee0880538e9808dd2cb26
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 13:13:31 +0900

    filter firstfiltertestservlet

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section01/filter/FirstFilterTestServlet.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section01/filter/FirstFilterTestServlet.java
@@ -5,16 +5,29 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/")
+@WebServlet("/first/filter")
 public class FirstFilterTestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+        System.out.println("servlet 요청 확인");
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=UTF-8");
 
+        PrintWriter out = response.getWriter();
+
+        out.println("<!doctype html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3>필터 확인용 서블릿 요청 확인 완료!</h3>");
+        out.println("</body>");
+        out.println("</html>");
+
+        out.flush();
+        out.close();
     }
 }
