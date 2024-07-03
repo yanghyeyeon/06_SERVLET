0fe659f1a26d9acd88b8eb15c94b7290ba88370b
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 13:15:04 +0900

    filter uses requestwrapper

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
@@ -1,2 +1,32 @@
-package com.ohgiraffers.section02.uses;public class RequestWrapper {
+package com.ohgiraffers.section02.uses;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class RequestWrapper extends HttpServletRequestWrapper {
+
+    // HttpServletRequestWrapper는 기본생성자가 없기 때문에, request를 전달해주는 생성자가 필요하다.
+    public RequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getParameter(String key) {
+
+        String value = "";
+        if ("password".equals(key)){
+
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+            // 암호화
+            value = passwordEncoder.encode(super.getParameter(key));
+
+            System.out.println("value ================>" + value);
+        } else {
+            value = super.getParameter(key);
+        }
+
+        return value;
+    }
 }
