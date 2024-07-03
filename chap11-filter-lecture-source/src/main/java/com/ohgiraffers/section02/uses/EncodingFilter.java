d36839876b4665fbaadefccf35c60ce697b8e35b
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 13:13:54 +0900

    filter uses encodingfilter

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/EncodingFilter.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/EncodingFilter.java
@@ -1,2 +1,35 @@
-package com.ohgiraffers.section02.uses;public class EncodingFilter {
+package com.ohgiraffers.section02.uses;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    private String encodingType;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encodingType = filterConfig.getInitParameter("encoding-type");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest hrequest = (HttpServletRequest) servletRequest;
+        if ("POST".equals(hrequest.getMethod())) {
+            System.out.println("기존 인코딩 타입 : " + hrequest.getCharacterEncoding());
+            hrequest.setCharacterEncoding("UTF-8");
+            System.out.println("변경된 인코딩 타입 : " + hrequest.getCharacterEncoding());
+        }
+
+        filterChain.doFilter(hrequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
 }
