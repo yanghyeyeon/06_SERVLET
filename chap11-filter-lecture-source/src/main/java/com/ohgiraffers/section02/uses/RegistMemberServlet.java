73c6fb07d65afdc8cb9a6f65c413e097a2231775
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 14:35:28 +0900

    filter uses registmemberservlet

--- chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
+++ chap11-filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RegistMemberServlet.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section02.uses;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.io.IOException;
 
@@ -18,5 +19,11 @@ public class RegistMemberServlet extends HttpServlet {
         System.out.println("userId = " + userId);
         System.out.println("password = " + password);
         System.out.println("name = " + name);
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println("비밀번호가 pass01인지 확인 : " +
+                passwordEncoder.matches("pass01",password));
+        System.out.println("비밀번호가 pass02인지 확인 : " +
+                passwordEncoder.matches("pass02",password));
     }
 }
