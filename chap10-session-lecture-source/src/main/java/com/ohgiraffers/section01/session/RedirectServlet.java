7c5f1157074c0514ba61a6f1c542296151e63374
yanghyeyeon <yhy4058@gmail.com>
2024-07-03 11:51:33 +0900

    session sessionhandlerservlet

--- chap10-session-lecture-source/src/main/java/com/ohgiraffers/section01/session/RedirectServlet.java
+++ chap10-session-lecture-source/src/main/java/com/ohgiraffers/section01/session/RedirectServlet.java
@@ -5,16 +5,54 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
-@WebServlet("/")
+@WebServlet("/redirect")
 public class RedirectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("(redirect) firstName : " + firstName);
+        System.out.println("(redirect) lastName : " + lastName);
 
+        HttpSession session = request.getSession();
+        System.out.println("redirect 페이지 session id : " + session.getId());
+
+        // 세션에 담긴 모든 Attribute 키 목록을 반환 받을 수 있다.
+        Enumeration<String> sessionNames = session.getAttributeNames();
+        while (sessionNames.hasMoreElements()) {
+            System.out.println(sessionNames.nextElement());
+        }
+
+        // 동일한 아이디를 가진 세션에서
+        // setAttribute해서 값을 지정할 수 있고
+        // getAttibute해서 값을 꺼내올 수 있다.
+        firstName = (String) session.getAttribute("firstName");
+        lastName = (String) session.getAttribute("lastName");
+
+        StringBuilder responseText = new StringBuilder();
+        responseText.append("<!doctype html>\n")
+                .append("<html>\n")
+                .append("<head>\n")
+                .append("</head>\n")
+                .append("<body>\n")
+                .append("<h3>your first name is ")
+                .append(firstName)
+                .append(" and last name is ")
+                .append(lastName)
+                .append("</h3>\n")
+                .append("</body>\n")
+                .append("</html>\n");
+
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print(responseText.toString());
+
+        out.flush();
+        out.close();
     }
 }
